f3b9d30f2dba75d827ee1eb33416642e71b4faca
Evan Miller <evanmiller067@gmail.com>
2021-01-17 19:08:09 -0800

    continued work on lab1

--- labs/lab1.cpp
+++ labs/lab1.cpp
@@ -2,11 +2,13 @@
 NAME: Evan Miller
 ASSIGNMENT: Lab 1
 CLASS: CS251
-SOURCES:
+SOURCES:http://www.cplusplus.com/reference/sstream/istringstream/istringstream/
 */
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
@@ -14,12 +16,44 @@ int main()
 {
 	string setA, setB;
 	cout << "Evan Miller \nLab 1 \nCS251 " << endl;
-
+	
+	//get setA from user, store in vector
 	cout << "Input the items in set A, separated by spaces.\nWhen you are done hit return. " << endl;
 	fflush(stdin);
-	getline(cin, setA, ' ');
-	//need to loop over cin to set all items in array of strings
-	cout << "\nItems in setA: " << setA << endl;
+	getline(cin, setA);
+	istringstream ssA(setA);
+	vector<string> vectorA;
+	for(string s;ssA>>s;)
+	{
+		vectorA.push_back(s);
+	}
+	
+	//get setB
+	cout << "\nInput the items in set B, separated by spaces. \nWhen you are done hit return. " << endl;
+	fflush(stdin);
+	getline(cin, setB);
+	istringstream ssB(setB);
+	vector<string> vectorB;
+	for(string s; ssB>>s;)
+	{
+		vectorB.push_back(s);
+	}
+
+	// test to see vector items
+	cout << "items in VectorA: " << endl;
+	int n = vectorA.size();	
+	for(int i = 0; i < n; i++)
+	{
+		cout << vectorA[i] << endl;
+	}
+	
+	cout << "Items in VectorB: " << endl;
+	int b = vectorB.size();
+	for(int i = 0; i < b; i++)
+	{
+		cout << vectorB[i] << endl;
+	}
+	//end of vector test
 
 	return 0;
 }
