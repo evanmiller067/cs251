2c4f3f00f2fce8fe55a405f004213a0844a4a51e
Evan Miller <evanmiller067@gmail.com>
2021-01-21 23:49:34 -0800

    updating lab1

--- labs/lab1.cpp
+++ labs/lab1.cpp
@@ -27,7 +27,16 @@ int main()
 	{
 		vectorA.push_back(s);
 	}
-	
+	//output set a
+	cout << "The following is a list of the items in set A: " << endl;
+	cout << "A = { " << flush;
+	int a = vectorA.size();	
+	for(int i = 0; i < a; i++)
+	{
+		cout << vectorA[i] << " ";
+	}
+	cout << "} \nSet A contains " << a << " items." << endl;
+
 	//get setB
 	cout << "\nInput the items in set B, separated by spaces. \nWhen you are done hit return. " << endl;
 	fflush(stdin);
@@ -38,22 +47,114 @@ int main()
 	{
 		vectorB.push_back(s);
 	}
-
-	// test to see vector items
-	cout << "items in VectorA: " << endl;
-	int n = vectorA.size();	
-	for(int i = 0; i < n; i++)
-	{
-		cout << vectorA[i] << endl;
-	}
-	
-	cout << "Items in VectorB: " << endl;
+	//output set  B
+	cout << "The following is a list of the items in set B: " << endl;
+	cout << "B = { " << flush;
 	int b = vectorB.size();
 	for(int i = 0; i < b; i++)
 	{
-		cout << vectorB[i] << endl;
+		cout << vectorB[i] << " ";
 	}
-	//end of vector test
+	cout << "} \nSet B contains " << b << " items." << endl;
+
+// A intersect B
+	int interCard = 0;	
+	cout << "\nA intersect B contains the following:\n{ ";
+	//intersect function
+	for(int i = 0; i < a; ++i)
+	{
+		for(int j = 0; j < b; ++j)
+		{
+			if(vectorA[i] == vectorB[j])
+			{
+				cout << vectorA[i] << " ";
+				++interCard;
+				break;
+			}
+		}
+	}
+	cout << "} \nThe cardinality of the intersection is " << interCard << endl;
+
+//A U B
+	int unionCard = a;
+	cout << "\nA U B contains the following: \n{ ";
+
+	int fin[25] = {0};
+	for(int i = 0; i < a; ++i)
+	{
+		cout << vectorA[i] << " ";
+		for(int j = 0; j < b; ++j)
+		{
+			if(vectorA[i] == vectorB[j])
+			{
+				fin[j] = i;
+			}
+		}
+		for(int i = 0; i < b; ++i)
+		{
+			if(!fin[i])
+			{
+				cout << vectorB[i] << " ";
+				++unionCard;
+			}
+		}
+	}
+	cout << "} \nThe cardinality of the union is " << unionCard << endl;	
+
+//A - B
+	int abCard = 0;
+	cout << "\nA - B = { " << flush;
+	for(int i = 0; i < a; ++i)
+	{
+		for(int j = 0; j < b; ++j)
+		{
+			if(vectorA[i] == vectorB[j])
+			{
+				break;
+			}
+			if(j == b)
+			{
+				cout << vectorA[i] << " ";
+				++abCard;
+			}
+		}
+	}
+	cout << " }\nThe cardinality is " << abCard << endl;
+
+//B - A
+	int baCard = 0;
+	cout << "\nB - A = { " << flush;
+	for(int i = 0; i < b; ++i)
+	{
+		for(int j = 0; j < a; ++j)
+		{
+			if(vectorB[i] == vectorA[j])
+			{
+				break;
+			}
+			if(j == a)
+			{
+				cout << vectorB[i] << " ";
+				++baCard;
+			}
+		}
+	}
+	cout << "}\nThe cardinality is " << baCard << endl;
+
+//A X B
+	int axbCard = a * b;
+	cout << "\nThe cardinality of A X B is " << axbCard << endl;
+	cout << "A X B contains { " << flush;
+	for(int i = 0; i < a; ++i)
+	{
+		for(int j = 0; j < b; ++j)
+		{
+			cout << "(" << vectorA[i] << ", " << vectorB[j] << ")";
+		}
+	}
+//powerset
+	int powerCard = a*b;
+	cout << "\nThe cardinality of the power set of the cross product is " << (1LL <<  powerCard) << endl;
 
 	return 0;
 }
